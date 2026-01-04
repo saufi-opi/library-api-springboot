fc30fe82483230dc909e4748c52a3495ed2b49be
Saufi <saufi1115@gmail.com>
2026-01-04 22:48:41 +0800

    feat: config and swagger tag

--- src/main/java/com/saufi/library_api/controller/RootController.java
+++ src/main/java/com/saufi/library_api/controller/RootController.java
@@ -1,10 +1,12 @@
 package com.saufi.library_api.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
+@Tag(name = "Root", description = "Root endpoint")
 public class RootController {
 
     @GetMapping("/")
