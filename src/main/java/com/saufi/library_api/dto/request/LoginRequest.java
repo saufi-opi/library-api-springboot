2cf5a715daa7b9d404bdea2234db957e488b2b79
Saufi <saufi1115@gmail.com>
2026-01-11 13:58:33 +0800

    feat: builder

--- src/main/java/com/saufi/library_api/dto/request/LoginRequest.java
+++ src/main/java/com/saufi/library_api/dto/request/LoginRequest.java
@@ -3,10 +3,12 @@ package com.saufi.library_api.dto.request;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class LoginRequest {
